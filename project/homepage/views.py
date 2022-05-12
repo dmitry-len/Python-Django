6fa1e3a17e7b7d0756793ff10d2a2eeb4f1de50c
len <71425931+dmitry-len@users.noreply.github.com>
2022-05-12 19:44:50 +1000

    Add files via upload

---
+++ project/homepage/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from icecream.models import icecream_db
+from anfisa.models import friends_db
+
+
+def index(request):
+    icecreams = ''
+    friends = ''
+
+    for friend in friends_db:
+        friends += f'{friend}<br>'
+
+    for i in range(len(icecream_db)):
+        icecreams += (f'{icecream_db[i]["name"]} | '
+                    f'<a href="{i}/">Узнать состав</a><br>')
+
+    context = {
+        'icecreams': icecreams,
+        'friends': friends,
+    }
+    return render(request, 'homepage/index.html', context)
